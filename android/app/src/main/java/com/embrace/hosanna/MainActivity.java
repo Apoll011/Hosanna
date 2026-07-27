352c237152de5562db8e725ed60b9777c2db84fd
apoll011 <tiagorobotik@gmail.com>
2026-07-27 17:20:16 +0100

    Add camera permission handling and integrate capacitor-camera dependency

--- android/app/src/main/java/com/embrace/hosanna/MainActivity.java
+++ android/app/src/main/java/com/embrace/hosanna/MainActivity.java
@@ -1,5 +1,21 @@
 package com.embrace.hosanna;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Allow WebView to access camera video streams
+        this.bridge.getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
